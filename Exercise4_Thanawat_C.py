1b962f5f7a42a8f3cba49e34fbd2f9a56a403890
Kenztor <k.kenmini@gmail.com>
2020-10-19 22:37:13 +0700

    Exercise4_Thanawat_C
    ปฏิบัติการทดสอบการใช้งานตัวแปร

---
+++ Exercise4_Thanawat_C.py
@@ -0,0 +1,14 @@
+x = 100.0 #Foundation English
+y = 90.0 #General Business
+z = 80.0 #Introduction to Computer Systems
+a = 70.0 #Computer Programming
+
+print("---Your Score--")
+
+print("Foundation English : ",x)
+
+print("General Business : ",y)
+
+print("Introduction to Computer Systems : ",z)
+
+print("Computer Programming : ",a)
